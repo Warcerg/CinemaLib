3f154f451abc802598de4eb2f04bec5ada67ebbb
warcerg <warcerg@gmail.com>
2021-08-15 21:19:14 +0300

    Implemented RecyclerView+Adapter

--- app/src/main/java/com/example/cinemalib/framework/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/cinemalib/framework/ui/main/MainFragment.kt
@@ -1,17 +1,17 @@
 package com.example.cinemalib.framework.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import com.example.cinemalib.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cinemalib.databinding.MainFragmentBinding
 import com.example.cinemalib.model.AppState
 import com.example.cinemalib.model.entities.MovieCard
+import com.example.cinemalib.model.entities.getDefaultMovie
 import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -20,12 +20,33 @@ class MainFragment : Fragment() {
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
 
+    private val movCard1 = MovieCard(getDefaultMovie(),5 , )
+    private val movCard2 = MovieCard(getDefaultMovie(),5 , )
+    private val movCard3 = MovieCard(getDefaultMovie(),5 , )
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = MainFragmentBinding.inflate(inflater, container, false)
+        initRecyclerView()
         return binding.root
+    }
+
+    private fun initRecyclerView() {
+        val recyclerView: RecyclerView = _binding!!.recyclerViewMovies
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        var MovieList: List<MovieCard> = listOf(movCard1, movCard2, movCard3, movCard1, movCard1, movCard1, movCard1, movCard1, )
+        recyclerView.adapter = MovieListAdapter(MovieList)
+
+        val recyclerView2:RecyclerView = _binding!!.recyclerViewMovies2
+        recyclerView2.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        recyclerView2.adapter = MovieListAdapter(MovieList)
     }
 
 
