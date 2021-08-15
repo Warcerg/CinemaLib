67758bcf3437f690c26a3dfec786d1494decdc76
warcerg <warcerg@gmail.com>
2021-08-15 14:07:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cinemalib/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.cinemalib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cinemalib.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
